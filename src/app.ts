c5d64e5023e5c28a90635e4e0fa0894d5f4928d2
farrelje <farrel.burns@gmail.com>
2021-08-17 17:24:13 +1200

    Complete generics examples

--- src/app.ts
+++ src/app.ts
@@ -1,137 +1,107 @@
-type Admin = {
-  name: string;
-  privileges: string[];
+// Built-in generics
+const names: Array<string> = ["Farrel", "Gary"];
+// const p: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Done");
+//   }, 2000)
+// })
+
+// p.then( data => {
+//   console.log(data.split(""));
+// })
+
+// Custom generics
+
+// Generics show that these objects will be of different types, unlike object
+// Used for when you don't care about anything other than that they will definitely be different
+// TypeScript is good at inferring the types this way
+// To constrain the types further, use extends
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-type User = {
-  name: string;
-  startDate: Date;
+const mergedObj = merge({name: "Farrel"}, {age: "unknown"});
+console.log(mergedObj.name);
+
+
+// Let's make it more versatile: don't care much about what comes in, but it must have a length property
+interface Lengthy {
+  length: number;
 }
 
-// Intersection types
-// Simpler than using interfaces for this reason
-type SuperUser = Admin & User;
-
-const u1: SuperUser = {
-  name: "Bastian",
-  privileges: ['read-write'],
-  startDate: new Date()
-}
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-type NullableNumber = number | null;
-type Universal = Combinable & Numeric;
-
-// Type guards
-
-type UnknownUser = User | Admin;
-
-function printUserInfo(user: UnknownUser) {
-  // Check for properties in object
-  if('privileges' in user) {
-    console.log(`Name: ${user.name}; ${user.privileges}`)
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let desc = "No value.";
+  if(element.length > 0) {
+    desc = `Length: ${element.length}`;
   }
-  if('startDate' in user) {
-    console.log(`Name: ${user.name}; ${user.startDate}`)
+  return [element, desc];
+}
+
+console.log(countAndDescribe(["Hi there!", "Cats", "Bananas"]));
+
+// Dependency generic: T is object type, and U is some sort of key from T
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
+}
+// Error - it's not a keyof
+// extractAndConvert({banana: "Farrel"}, "name");
+extractAndConvert({name: "Farrel"}, "name");
+
+// Generic storage class
+// Rather than having to create multiple versions or unions
+// we can dynamically create different versions to be resolved later
+// This gives us massive flexibility and type safety
+
+type Primitive = string | number | boolean;
+class DataStorage<T extends Primitive> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    return [...this.data, item];
+  }
+  removeItem(item: T) {
+    return this.data.filter(i => i !== item);
+  }
+
+  getItems() {
+    return [...this.data];
   }
 }
 
-printUserInfo(u1);
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Cats");
+// Nope:
+//textStorage.addItem(2);
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(2);
 
-type Vehicle = Car | Truck;
-class Car  {
-  drive() {
-    console.log("Driving");
-  }
+// However, falls apart a bit here, as functions are set up for primitives and primitive comparisons
+// Hence, we extend primitives to avoid this issue
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({name: "Farrel"});
+
+// More built-ins
+interface CourseGoal {
+  title: string;
+  description: string;
+  dueDate: Date;
 }
 
-class Truck  {
-  drive() {
-    console.log("Driving");
-  }
-  loadCargo() {
-    console.log("Loading cargo");
-  }
+function createCourseGoal(title: string, description: string, dueDate: Date): CourseGoal {
+  // Partial allows temporary optionally, e.g. here where building an object
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.dueDate = dueDate;
+  return courseGoal as CourseGoal;
 }
 
-const car = new Car();
-const v2 = new Truck();
+const names2: Readonly<string[]> = ["Dave", "Gavin"];
+// Now this won't work
+// names2.push("Cat");
 
-// Instanceof great with classes
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  if(vehicle instanceof Truck) {
-    vehicle.loadCargo();
-  }
-}
-
-// Discriminated unions
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-// Can't use instanceof with interfaces
-function moveAnimal(animal: Animal) {
-  let speed: number;
-  switch(animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-      break;
-  }
-  console.log("Moving at: " + speed);
-}
-
-moveAnimal({type: 'bird', flyingSpeed: 20});
-
-// Type casting
-// Either/or
-// const input = <HTMLInputElement>document.getElementById('userInput')!;
-const input = document.getElementById('userInput')! as HTMLInputElement;
-
-input.value = "Hi there!";
-
-// Index properties - don't know exact key:value name, but can map string to string
-// Useful for error handling or dynamic general type
-// Cannot use id as number below
-interface ErrorContainer {
-  // index type
-  [key: string]: string
-}
-
-const errorBag: ErrorContainer = {
-  "email": "Not a valid email.",
-  "username": "Must be between 2 an 50 characters."
-}
-
-// Function overloads
-function add(a: string, b: string): string
-function add(a: number, b: string): number
-function add(a: number, b: number): number
-function add(a: Combinable, b: Combinable) {
-  if(typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString()
-  }
-  return a + b;
-}
-
-// Optional chaining
-
-const fetchedData = {
-  id: "1",
-  name: "Farrel",
-  job: {title: "CEO", description: "Company X"}
-}
-
-console.log(fetchedData?.job?.title);
+// Summary: generics vs unions
+// - Unions: (string | number | boolean) [] would mean a MIXED array of any of these types, which isn't great
+//  ANY of these types are accepted
+// - Generics: T extends string | number | boolean
+//  This means, however, that you can only choose ONE in each instance, which is much better
